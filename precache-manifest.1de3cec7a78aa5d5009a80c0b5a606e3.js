3cff43a2c72a963e1c8a9dad24357eb48ae1e402
Grantas Gadliauskas <grantas0612@gmail.com>
2019-12-28 23:53:35 +0200

    Updates

---
+++ precache-manifest.1de3cec7a78aa5d5009a80c0b5a606e3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c061590c84ea23f5e581964e5f0c84bc",
+    "url": "/Doomsday-algorithm-practice/index.html"
+  },
+  {
+    "revision": "6c1dd582dc37abaf8918",
+    "url": "/Doomsday-algorithm-practice/static/js/2.f59ceac0.chunk.js"
+  },
+  {
+    "revision": "d3cf03907af10eb31610",
+    "url": "/Doomsday-algorithm-practice/static/js/main.a17b176a.chunk.js"
+  },
+  {
+    "revision": "644391aec1fd369201b5",
+    "url": "/Doomsday-algorithm-practice/static/js/runtime-main.2fb2fff5.js"
+  }
+]);
